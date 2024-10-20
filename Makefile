0a17f543ce172029d815f0a16bdf1870fbb26c24
penglei <penglei@ybyte.org>
2024-10-20 23:28:33 +0800

    sops template example

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+default:
+	echo "please provide the target."
+
 pin-registry:
 	@nix registry pin nixpkgs "github:NixOS/nixpkgs/$$(cat flake.lock|jq -r '.nodes[.nodes.root.inputs.nixpkgs].locked.rev')"
 	@nix registry pin home-manager github:nix-community/home-manager/$$(cat flake.lock|jq -r '.nodes["home-manager"].locked.rev')
@@ -5,3 +8,6 @@ pin-registry:
 sops-update:
 	sops updatekeys secrets/basic.yaml 
 	sops updatekeys secrets/server.yaml
+
+switch:
+	sudo nixos-rebuild switch --flake .
--- nixos/modules/ssserver.nix
+++ nixos/modules/ssserver.nix
@@ -1,16 +1,30 @@
 { pkgs, config, ... }:
 
 let
-  #configFile = config.sops.templates.ssserver.path;
   configFile = config.sops.secrets."ssserver.json".path;
+  generatedConfigFile = config.sops.templates."ss-server-config.json".path;
+  configTemplate = ''
+    {
+      "server": "0.0.0.0",
+      "server_port": 80,
+      "method" : "chacha20-ietf-poly1305",
+      "password": "${config.sops.placeholder.ss-password}"
+      "timeout" : 300
+    }
+  '';
 in {
-  sops = {
-    templates.ssserver.content = builtins.toJSON { "server_port" = 8388; };
+  sops.templates."ss-server-config.json" = {
+    content = configTemplate;
+    mode = "0400";
   };
 
-  sops.secrets."ssserver.json" = {
-    sopsFile = ../../secrets/server.yaml;
-    restartUnits = [ "ssserver.service" ];
+  sops.secrets = let sfile = ../../secrets/server.yaml;
+  in {
+    "ssserver.json" = {
+      sopsFile = sfile;
+      restartUnits = [ "ssserver.service" ];
+    };
+    "ss-password" = { sopsFile = sfile; };
   };
   systemd.services.ssserver = {
     description = "ssserver daemon";
@@ -18,6 +32,7 @@ in {
     wantedBy = [ "multi-user.target" ];
     path = [ pkgs.shadowsocks-rust pkgs.v2ray-plugin ];
     script = ''
+      echo "sops template example file: ${generatedConfigFile} "
       exec ssserver -c ${configFile}
     '';
     serviceConfig = { WorkingDirectory = "/tmp"; };
