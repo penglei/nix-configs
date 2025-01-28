c1b313702cfcb5ef17047f84b61222339a712810
penglei <penglei@ybyte.org>
2025-01-28 18:01:40 +0800

    fix space windows stack mode icon

--- files/dotfiles/.config/sketchybar/plugins/yabai_space_state.sh
+++ files/dotfiles/.config/sketchybar/plugins/yabai_space_state.sh
@@ -5,13 +5,13 @@ space_mode=$(yabai -m query --spaces --space | jq -r .type)
 # echo space_mode:$space_mode >> $HOME/debug
 
 case "$space_mode" in
-    bsp)
-    sketchybar -m --set space_mode label=""
-    ;;
-    stack)
-    sketchybar -m --set space_mode label="﯅"
-    ;;
-    float)
-    sketchybar -m --set space_mode label=""
-    ;;
+bsp)
+  sketchybar -m --set space_mode label=""
+  ;;
+stack)
+  sketchybar -m --set space_mode label=""
+  ;;
+float)
+  sketchybar -m --set space_mode label=""
+  ;;
 esac
