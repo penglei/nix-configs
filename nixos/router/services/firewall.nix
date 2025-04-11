31994eb4713807e79cc0d0b7db482e1877e50c24
penglei <penglei@ybyte.org>
2025-04-12 01:46:33 +0800

    up

---
+++ nixos/router/services/firewall.nix
@@ -0,0 +1,39 @@
+{ config, ... }: {
+
+  # networking.firewall.extraCommands = with pkgs.lib; ''
+  #   ${pkgs.nftables}/bin/nft -f - <<EOF
+  #   table inet ab-forward;
+  #   flush table inet ab-forward;
+  #   table inet ab-forward {
+  #     chain FORWARD {
+  #       type filter hook forward priority filter; policy drop;
+  #       ct state related,established accept;
+  #       ${optionalString null ""}
+  #     }
+  #   }
+  #   EOF
+  # '';
+
+  networking.firewall = {
+    enable = true;
+    logRefusedConnections = false;
+    logRefusedPackets = false;
+    allowedTCPPorts = [ 80 443 22 ]; # extras
+    extraInputRules = ''
+      iifname != ${config.netaddr.iface.wan.name} accept
+    '';
+    filterForward = false;
+    checkReversePath = false;
+  };
+
+  networking.nftables.tables.miniupnpd = {
+    family = "inet";
+    content = ''
+      chain forward {
+        type filter hook forward priority filter; policy accept;
+        jump miniupnpd
+      }
+    '';
+  };
+}
+
