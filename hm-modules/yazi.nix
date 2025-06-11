49f1a593c77fd1745ced233631670649749ef24d
penglei <penglei@ybyte.org>
2025-06-11 10:53:24 +0800

    fix yazi

--- hm-modules/yazi.nix
+++ hm-modules/yazi.nix
@@ -1,26 +1,42 @@
-{ pkgs, lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 
 #docs: https://yazi-rs.github.io/features/
 {
   xdg.configFile = {
     "yazi/theme.toml" = {
-      source =
-        ../files/dotfiles/_config/yazi/themes/catppuccin/macchiato/catppuccin-macchiato-sky.toml;
+      source = ../files/dotfiles/_config/yazi/themes/catppuccin/macchiato/catppuccin-macchiato-sky.toml;
     };
-    "yazi/Catppuccin-macchiato.tmTheme" = let
-      batTheme = config.programs.bat.themes;
-      name = "catppuccin";
-    in lib.mkIf (lib.hasAttr name batTheme) {
-      source = let val = batTheme."${name}"; in "${val.src}/${val.file}";
-    };
+    "yazi/Catppuccin-macchiato.tmTheme" =
+      let
+        batTheme = config.programs.bat.themes;
+        name = "catppuccin";
+      in
+      lib.mkIf (lib.hasAttr name batTheme) {
+        source =
+          let
+            val = batTheme."${name}";
+          in
+          "${val.src}/${val.file}";
+      };
   };
-  home.shellAliases = { "y" = "yazi"; };
+  home.shellAliases = {
+    "y" = "yazi";
+  };
   programs.yazi = {
     enable = true;
     shellWrapperName = "yy";
     settings = {
-      manager = {
-        ratio = [ 1 2 4 ];
+      mgr = {
+        ratio = [
+          1
+          2
+          4
+        ];
         sort_by = "natural";
         sort_dir_first = true;
       };
@@ -87,30 +103,49 @@
         run = "plugin bookmarks --args=jump";
       }
       {
-        on = [ "b" "d" ];
+        on = [
+          "b"
+          "d"
+        ];
         run = "plugin bookmarks --args=delete";
       }
 
       #fg
       {
-        on = [ "f" "g" ];
+        on = [
+          "f"
+          "g"
+        ];
         run = "plugin fg";
         desc = "find file by content (fuzzy match)";
       }
       {
-        on = [ "f" "G" ];
+        on = [
+          "f"
+          "G"
+        ];
         run = "plugin fg --args='rg'";
         desc = "find file by content (ripgrep match)";
       }
       {
-        on = [ "f" "f" ];
+        on = [
+          "f"
+          "f"
+        ];
         run = "plugin fg --args='fzf'";
         desc = "find file by filename";
       }
     ];
     initLua = ../files/dotfiles/_config/yazi/init.lua;
     plugins = {
-      inherit (pkgs.yazi-plugins) full-border git max-preview lsar bookmarks fg;
+      inherit (pkgs.yazi-plugins)
+        full-border
+        git
+        max-preview
+        lsar
+        bookmarks
+        fg
+        ;
       "smart-paste" = ../files/dotfiles/_config/yazi/plugins/smart-paste.yazi;
       "arrow" = ../files/dotfiles/_config/yazi/plugins/arrow.yazi;
     };
