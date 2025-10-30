ff50e7db4655eede0e621d427fef06005e9fb3fc
penglei <penglei@ybyte.org>
2025-10-30 10:14:37 +0800

    flake update

--- hm-modules/git.nix
+++ hm-modules/git.nix
@@ -2,71 +2,62 @@
 {
   programs.git = {
     enable = true;
-    userEmail = "lambit@qq.com";
-    userName = "penglei";
-    aliases = {
-      # Prettier `git log` https://git-scm.com/docs/pretty-formats
-      lg = "log --color --pretty=format:'%Cred%h%Creset %G? -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      # `git checkout` with fuzzy matching
-      co = "!git for-each-ref --format='%(refname:short)' refs/heads | ${pkgs.fzf}/bin/fzf -0 | xargs git checkout";
-      # `git restore --staged` with fuzzy matching
-      rs = "!git diff --name-only --cached | ${pkgs.fzf}/bin/fzf -0 --multi | xargs git restore --staged";
-      # Fetch a branch from a remote and rebase it on the current branch
-      frb = "!git fetch $1 && git rebase $1/$2 && :";
+    settings = {
+      user = {
+        name = "penglei";
+        email = "lambit@qq.com";
+      };
+      alias = {
+        # Prettier `git log` https://git-scm.com/docs/pretty-formats
+        lg = "log --color --pretty=format:'%Cred%h%Creset %G? -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+        # `git checkout` with fuzzy matching
+        co = "!git for-each-ref --format='%(refname:short)' refs/heads | ${pkgs.fzf}/bin/fzf -0 | xargs git checkout";
+        # `git restore --staged` with fuzzy matching
+        rs = "!git diff --name-only --cached | ${pkgs.fzf}/bin/fzf -0 --multi | xargs git restore --staged";
+        # Fetch a branch from a remote and rebase it on the current branch
+        frb = "!git fetch $1 && git rebase $1/$2 && :";
 
-      new = "checkout -b";
-      rb = "rebase --interactive";
-      last = "show HEAD";
-      cim = "commit --amend";
-      cimn = "commit --amend --no-edit";
-      st = "status";
-      br = "branch";
-      ci = "commit";
-      df = "diff";
-      ps = "push";
-      fp = "push --force";
-      track = "checkout --track";
-      cliff = "!${pkgs.git-cliff}/bin/git-cliff";
-      whois = ''
-        !sh -c 'git log -i -1 --pretty="format:%an <%ae>
-        " --author="$1"' -'';
-      whatis = "show -s --pretty='tformat:%h (%s, %ad)' --date=short";
+        new = "checkout -b";
+        rb = "rebase --interactive";
+        last = "show HEAD";
+        cim = "commit --amend";
+        cimn = "commit --amend --no-edit";
+        st = "status";
+        br = "branch";
+        ci = "commit";
+        df = "diff";
+        ps = "push";
+        fp = "push --force";
+        track = "checkout --track";
+        cliff = "!${pkgs.git-cliff}/bin/git-cliff";
+        whois = ''
+          !sh -c 'git log -i -1 --pretty="format:%an <%ae>
+          " --author="$1"' -'';
+        whatis = "show -s --pretty='tformat:%h (%s, %ad)' --date=short";
 
-      dlog = "-c diff.external=difft log --ext-diff";
-      dshow = "-c diff.external=difft show --ext-diff";
-      ddiff = "-c diff.external=difft diff";
-      dd = "-c diff.external=difft diff";
+        dlog = "-c diff.external=difft log --ext-diff";
+        dshow = "-c diff.external=difft show --ext-diff";
+        ddiff = "-c diff.external=difft diff";
+        dd = "-c diff.external=difft diff";
+      };
 
+      extraConfig = {
+        pager.branch = false;
+        pull.rebase = true;
+        init.defaultBranch = "main";
+        push.default = "current";
+        merge.conflictstyle = "zdiff3";
+        diff.colorMoved = "default";
+        core = {
+          hooksPath = "${config.xdg.configHome}/git/hooks";
+          quotepath = false;
+        };
+      };
     };
     signing = {
       signByDefault = false;
       #key = null; # Let the gpg agent handle it
       key = (import ../config.nix).gpg.sign_key + "!";
-    };
-
-    extraConfig = {
-      pager.branch = false;
-      pull.rebase = true;
-      init.defaultBranch = "main";
-      push.default = "current";
-      merge.conflictstyle = "zdiff3";
-      diff.colorMoved = "default";
-      core = {
-        hooksPath = "${config.xdg.configHome}/git/hooks";
-        quotepath = false;
-      };
-    };
-
-    # Prettier pager, adds syntax highlighting and line numbers
-    # see: https://github.com/dandavison/delta
-    delta = {
-      enable = true;
-      options = {
-        navigate = true;
-        line-numbers = true;
-        conflictstyle = "diff3";
-        side-by-side = true;
-      };
     };
 
     ignores = [
@@ -101,6 +92,19 @@
       ".luarc.json"
     ];
 
+  };
+
+  # Prettier pager for git, adds syntax highlighting and line numbers
+  # see: https://github.com/dandavison/delta
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      navigate = true;
+      line-numbers = true;
+      conflictstyle = "diff3";
+      side-by-side = true;
+    };
   };
 
   home.shellAliases = {
